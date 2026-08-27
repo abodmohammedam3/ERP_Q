bb18484c5b31683db6a36e652d4b6ba0c470b77b
abodmohammedam3 <abodmohammedam3@gmail.com>
2026-08-27 17:55:04 +0300

    دخول تلقائي لللداش بورد

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 use Illuminate\Support\Facades\Route;
+Route::get('/', function () {
+    return view('dashboard.index');
+});
 
 Route::get('/dashboard', function () {
     return view('dashboard.index');
